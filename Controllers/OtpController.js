fa6ca5b2f33d3380c1189dd98723423261febc9c
ravikumar197 <ravikumarmenthula@example.com>
2024-08-23 15:31:29 +0530

    first commit

---
+++ Controllers/OtpController.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ravikumarmenthula@gmail.com',
+    pass: 'eyln ssax buxj kesc',
+  },
+});
+
+const sendOtp = async (email, req) => {
+  if (!email) {
+    return null;
+  }
+
+  const otp = crypto.randomInt(100000, 999999).toString();
+  const mailOptions = {
+    from: 'ravikumarmenthula@gmail.com',
+    to: email,
+    subject: 'Your OTP Code',
+    text: `Your OTP code is: ${otp}`,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+
+    const currentTime = new Date();
+    const otpData = { otp, currentTime };
+    req.app.locals.otpData = otpData;
+
+    return otpData;
+  } catch (error) {
+    console.error('Failed to send OTP', error);
+    return null;
+  }
+};
+
+module.exports = { sendOtp };
